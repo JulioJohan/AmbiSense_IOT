992557d9dd07b19df0ff671d1166a00d0bde2757
JulioJohan <juliojohan10@gmail.com>
2023-08-18 20:03:29 -0600

    Agregando codigo fuente

--- codigo_fuente/Codigo_ESP32.py
+++ codigo_fuente/Codigo_ESP32.py
@@ -2,9 +2,19 @@ import time
 import network
 from umqtt.simple import MQTTClient
 from machine import Pin
+import machine
 import dht
 import neopixel
 
+#Pines de sonido
+pin_analogo = machine.ADC(machine.Pin(32))  # Cambia el número de pin según corresponda
+pin_digital = machine.ADC(machine.Pin(33))  # Cambiado a pin 27
+
+#Pin de fotoResistencia
+digital_resistencia_pin = machine.ADC(machine.Pin(34))  # Cambiado a pin 27
+
+# Configurar el pin del zumbador o altavoz como PWM
+buzzer_pin = machine.PWM(machine.Pin(22))
 
 # Pines y variables para PIR.
 motion = False
@@ -18,7 +28,7 @@ pin_sensor = Pin(35, Pin.IN)
 # Motor
 pin_motor = Pin(27, Pin.OUT)
 #LED RGB
-neo_rojo = neopixel.NeoPixel(Pin(23), 256)
+#neo_rojo = neopixel.NeoPixel(Pin(23), 256)
 neo_azul = neopixel.NeoPixel(Pin(21), 256)
 
 # Configuraciones de MQTT	
@@ -30,6 +40,9 @@ MQTT_TOPIC_1 = "utng/pir"
 MQTT_TOPIC_2 = "utng/gas"
 MQTT_TOPIC_3 = "utng/temp"
 MQTT_TOPIC_4 = "utng/humedad"
+MQTT_TOPIC_5 = "utng/sonido"
+MQTT_TOPIC_6 = "utng/fotoresistencia"
+
 
 MQTT_PORT = 1883
 
@@ -38,12 +51,43 @@ MQTT_PORT = 1883
 def sensor_temp():
     sensor.measure()
     temp = sensor.temperature()
+    print("temperatura",temp)
     return round(temp,1)
 
 # Medición de Humedad
 def sensor_hum():
     hum = sensor.humidity()
+    print("humedad",hum)
+    print(hum)
     return round(hum, 1)
+
+# Función para reproducir un tono
+def play_tone(frequency, duration):
+    buzzer_pin.freq(frequency)
+    buzzer_pin.duty(512)
+    time.sleep(duration)
+    buzzer_pin.duty(0)
+
+def sensor_sound():
+     # Lee el valor analógico del sensor
+    analog_value = pin_analogo.read_u16() // 10
+
+    # Lee el valor digital del sensor (lectura analógica para pines digitales también)
+    digital_value = pin_digital.read_u16() // 10
+
+    time.sleep(1)  # Espera 1 segund
+    
+    return str(digital_value)
+
+def sensor_foto_resistencia():
+    # Lee el valor digital del sensor (lectura analógica para pines digitales también)
+    digital_value = digital_resistencia_pin.read() 
+    # Imprime los valores
+    print("Valor digital:", digital_value)
+
+    time.sleep(1)  # Espera 1 segund
+    
+    return str(digital_value)
 
 
 # Función para conectar al WiFi
@@ -53,7 +97,7 @@ def wifi_connect():
     sta_if.active(True)
     
     if not sta_if.isconnected():
-        sta_if.connect('LAPTOP-EEKAKGFK 7335', '12345678')
+        sta_if.connect('POCO X3 Pro', '12345678')
         while not sta_if.isconnected():
             pass
     print("WiFi conectado:", sta_if.ifconfig())
@@ -86,6 +130,7 @@ def leer_sensor():
     lectura = pin_sensor.value()
     if lectura == 1:
         pin_motor.value(1)
+        play_tone(1000, 0.5)  # Reproducir un tono de 1000 Hz durante 0.5 segundos
         print("Se detectó gas de hidrógeno")
         # Aquí puedes agregar las acciones que deseas realizar cuando se detecte el gas de hidrógeno
     else:
@@ -101,10 +146,25 @@ def publishTempHum(temp, hum):
     print("[OK]")
     client.publish(MQTT_TOPIC_3, str(temp))
     print("Publicando temperatura: ", temp, "ºC")
-
     client.publish(MQTT_TOPIC_4, str(hum))
     print("Publicando humedad: ", hum, "%")
     
+def publishSound(sound):
+    client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASSWORD, keepalive=30)
+    client.connect()
+    print("[OK]")
+    client.publish(MQTT_TOPIC_5, str(sound))
+    print("Sonido:", sound)
+    client.disconnect()
+
+def publishFotoResistencia(foto_resistencia):
+    client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASSWORD, keepalive=30)
+    client.connect()
+    print("[OK]")
+    client.publish(MQTT_TOPIC_6, str(foto_resistencia))
+    print("FotoResistencia:", foto_resistencia)
+    client.disconnect() 
+
 def alerta_policia():
     for i in range(5):
         for i in range(n):
@@ -127,6 +187,7 @@ pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 
 # Programa principal
 wifi_connect()
+#Temperatura y Humedad
 sensor = dht.DHT11(Pin(5))
 
 
@@ -138,6 +199,9 @@ while True:
         time.sleep(4)
         valor = leer_sensor()
         publishGas(valor)
+        sound = sensor_sound()
+        foto_resistencia = sensor_foto_resistencia()
+        print("Valor foto_resistencia",foto_resistencia)
         if motion:
             print("Movimiento detectado")
             alerta_policia()
@@ -146,5 +210,5 @@ while True:
             motion = False
     except Exception as e:
         print("Error:", e)
-        wifi_connect()
+        #wifi_connect()
 con
